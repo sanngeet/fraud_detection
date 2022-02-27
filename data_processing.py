97e15e5705665e2d32ba275b16f1d9cf4c9fdb89
sanngeet <sanngeetbargotra@gmail.com>
2022-02-27 17:17:17 +0530

    added data processing

--- data_processing.py
+++ data_processing.py
@@ -1,12 +1,1 @@
-print("Adding all the data processing code related to Fraud Detection.")
-
-a = 5
-b = 7
-
-
-def sum(a, b):
-    c = a + b
-    return c
-
-
-print("Added a new function sum which is not correct")
+print("Adding all the data processing code related to fraud detection.")
